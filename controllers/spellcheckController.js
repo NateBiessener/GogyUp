19b7ee460fbf368f53152040484b4fad40488ff7
Matthew Steinbergs <mj.steinbergs@gmail.com>
2016-10-21 10:26:37 -0500

    updated folder structure

--- controllers/spellcheckController.js
+++
@@ -1,7 +0,0 @@
-var myApp = angular.module('myApp', []);
-
-myApp.controller('spellcheckController', ['$scope', function($scope){
-
-  $scope.enteredWord = 'especially';
-
-}]); // end spellcheckController
