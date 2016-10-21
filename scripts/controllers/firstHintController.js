226f9da4cc1b7054baf7b98ef994bea73e339124
Matthew Steinbergs <mj.steinbergs@gmail.com>
2016-10-21 11:29:35 -0500

    first hint complete

---
+++ scripts/controllers/firstHintController.js
@@ -0,0 +1,7 @@
+myApp.controller('firstHintController', ['$scope', function($scope){
+  console.log('in firstHintController');
+
+  $scope.dummyArray = ['a', 'b', 'c', 'd'];
+
+  
+}]); // end firstHintController
--- scripts/controllers/spellcheckController.js
+++ scripts/controllers/spellcheckController.js
@@ -1,14 +1,16 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('spellcheckController', ['$scope', function($scope){
+myApp.controller('spellcheck', ['$scope', function($scope){
 
   $scope.enteredWord = 'especially';
-  $scope.receivedWord = 'especially';
+  console.log($scope.enteredWord);
+  $scope.anotherWord = 'crap';
+  console.log($scope.anotherWord);
 
   $scope.checkSpelling = function(){
     console.log('in $scope.checkSpelling');
 
-    if($scope.enteredWord === $scope.receivedWord){
+    if($scope.enteredWord === $scope.anotherWord){
       alert('You win!');
     } else {
       alert('Try again!');
