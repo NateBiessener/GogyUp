38bfa3f3b1af1023f3c18adf4bc65198628b5174
Nate Biessener <nathaniel.biessener@gmail.com>
2016-10-21 10:03:39 -0500

    file structure maintenance

---
+++ readerEmulator/server/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+var path = require('path');
+
+app.listen(3141, function(){
+  console.log('server up on 3141');
+});
+
+app.get('/', function(req, res){
+  console.log('base url hit');
+  res.sendFile(path.resolve('gogyUpReaderEmulator/public/index.html'));
+});
+
+app.use(express.static('gogyUpReaderEmulator/public'));
