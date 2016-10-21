a888f91dba65ff5bac10af1e9f74b8a392133e8d
Matthew Steinbergs <mj.steinbergs@gmail.com>
2016-10-21 10:13:45 -0500

    spellcheckController set up/angular sourced in dummy view

---
+++ controllers/spellcheckController.js
@@ -0,0 +1,7 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('spellcheckController', ['$scope', function($scope){
+
+  $scope.enteredWord = 'especially';
+
+}]); // end spellcheckController
