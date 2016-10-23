94e851241f5c2eb33ce2082c141d182c5cacafa2
Matthew Steinbergs <mj.steinbergs@gmail.com>
2016-10-23 18:48:10 -0500

    hint display functionally complete

--- scripts/controllers/secondHintController.js
+++ scripts/controllers/secondHintController.js
@@ -1,22 +1,37 @@
 myApp.controller('secondHintController', ['$scope', function($scope){
   console.log('in secondHintController');
 
-  $scope.graphemeWord = ['g', 'r', 'ea', 'se'];
-
   $scope.dummyWord = "grease";
   $scope.dummyArray = $scope.dummyWord.split("");
+  var dummyArray = $scope.dummyArray;
   console.log($scope.dummyArray);
 
   $scope.targetGrapheme = "ea";
   $scope.splitGrapheme = $scope.targetGrapheme.split("");
+  var splitGraph = $scope.splitGrapheme;
   console.log($scope.splitGrapheme);
 
   var Graph = $scope.targetGrapheme;
   var Word = $scope.dummyWord;
   console.log(Graph, Word);
 
-  Word.indexOf(Graph);
-  console.log(Word.indexOf(Graph));
+  var graphemeIndex = Word.indexOf(Graph);
+  console.log(graphemeIndex);
+
+  $scope.placeGrapheme = function(){
+    var domArray = [];
+    j = 0;
+    for (var i = 0; i < dummyArray.length; i++) {
+      if(i >= graphemeIndex && i <= (graphemeIndex + (Graph.length - 1))){
+        domArray.push(splitGraph[j]);
+        j++;
+      } else {
+        domArray.push("_");
+      }
+    }
+    $scope.domArray = domArray;
+    console.log($scope.domArray);
+  };
 
   // $scope.placeGrapheme = function(array, target){
   //   console.log('in $scope.placeGrapheme');
