696fa74012993617b0dc655ee58eff539e816f5e
Matthew Steinbergs <mj.steinbergs@gmail.com>
2016-10-24 11:59:49 -0500

    angular route structure

--- scripts/client.js
+++ scripts/client.js
@@ -1,22 +1,22 @@
 var myApp = angular.module('myApp', []);
 
-// // angular routing
-// myApp.config(['$routeProvider', function($routeProvider){
-//
-//   $routeProvider.
-//     when('/quoteMe', {
-//       templateUrl: '../views/partials/quoteMe.html',
-//       controller: 'quoteMeController'
-//     }).
-//     when('/quotationCreation', {
-//       templateUrl: '../views/partials/quotationCreation.html',
-//       controller: 'quotationCreationController'
-//     }).
-//     when('/quoteStash', {
-//       templateUrl: '../views/partials/quoteStash.html',
-//       controller: 'quoteStashController'
-//     }).
-//     otherwise({
-//       redirectTo: '/quoteMe'
-//     });
-// }]); // end angular routing
+// angular routing
+myApp.config(['$routeProvider', function($routeProvider){
+
+  $routeProvider.
+    when('/firstTry', {
+      templateUrl: '../views/firstTry.html',
+      controller: 'firstTryController'
+    }).
+    when('/secondTry', {
+      templateUrl: '../views/secondTry.html',
+      controller: 'secondTryController'
+    }).
+    when('/thirdTry', {
+      templateUrl: '../views/thirdTry.html',
+      controller: 'thirdTryController'
+    }).
+    otherwise({
+      redirectTo: '/firstTry'
+    });
+}]); // end angular routing
