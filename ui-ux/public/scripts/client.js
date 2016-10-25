b3ac8079843548f0895004adc8b9353109beb096
Ian Coldwater <ian@coldwater.io>
2016-10-25 18:29:11 -0500

    buttons and stuff

--- ui-ux/public/scripts/client.js
+++ ui-ux/public/scripts/client.js
@@ -1,6 +1,9 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-      $(document).ready(function() {
-      // $(".data-sentence").lettering('words').children('span').lettering().sortable();
-         $(".data-tiles").lettering().sortable();
+$(document).ready(function() {
+// $(".data-sentence").lettering('words').children('span').lettering().sortable();
+   $(".data-tiles").lettering().draggable();
+
+
+
 });
