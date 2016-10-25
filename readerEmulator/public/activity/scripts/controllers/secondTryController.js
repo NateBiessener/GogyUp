619056f3b3f0c33589f3f3e865a2f1fcdcdde0cd
BENJAMIN JOHNSON <wibajohnson@gmail.com>
2016-10-25 10:24:55 -0500

    hint for second try

--- readerEmulator/public/activity/scripts/controllers/secondTryController.js
+++ readerEmulator/public/activity/scripts/controllers/secondTryController.js
@@ -1,11 +1,17 @@
 myApp.controller('secondTryController', ['$scope', function($scope){
   console.log('in secondTryController');
-
+var correctWord = window.parent.stuff.word.fullWord;
   // spellcheck dummy words
   $scope.enteredWord = 'especially';
   console.log($scope.enteredWord);
   $scope.comparisonWord = 'crap';
   console.log($scope.anotherWord);
+ $scope.underline = "";
+  $scope.displayUnderline = function(){
+    for(var i = 0; i < correctWord.length; i++){
+$scope.underline += "_ ";
+    }
+  };
 
   // spellchecking function
   $scope.checkSpelling = function(){
