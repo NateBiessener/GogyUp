b7281cdca77212baa4c12a837fcf85b274bf9d8f
Nate Biessener <nathaniel.biessener@gmail.com>
2016-10-26 17:55:08 -0500

    initial style merge

---
+++ readerEmulator/public/activity/scripts/styleScript.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+$(document).ready(function() {
+// $(".data-sentence").lettering('words').children('span').lettering().sortable();
+   $(".data-tiles").lettering().draggable();
+
+
+
+});
