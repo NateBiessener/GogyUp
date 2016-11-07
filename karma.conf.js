172c48e8b15c5d5b75869de0b7dbd88fa2693cb7
Matthew Steinbergs <mj.steinbergs@gmail.com>
2016-11-07 09:37:25 -0600

    files linked in karma config

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'readerEmulator/public/activity/scripts/client.js',
+      'readerEmulator/public/activity/scripts/factories/SpellingFactory.js',
       'tests/**/*.js'
     ],
 
