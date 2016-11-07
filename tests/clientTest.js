56be3e9da9fecb765b3719ae68a66426e695d2d7
Matthew Steinbergs <mj.steinbergs@gmail.com>
2016-11-07 09:47:15 -0600

    initial client test

--- tests/clientTest.js
+++ tests/clientTest.js
@@ -1,4 +1,19 @@
 describe('mainController', function(){
   beforeEach(module('myApp'));
 
-  
+  describe('placeLetter()', function(){
+    it('place letter in array', inject(function($controller){
+      var scope = {};
+      console.log('here in test');
+
+      var firstTryController = $controller('firstTryController', {
+        $scope: scope
+
+      });
+
+      scope.placeLetter('X');
+
+      scope.placedWord.should.contain('X');
+    }));
+  });
+});
